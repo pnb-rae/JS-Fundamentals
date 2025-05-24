e95afba1d2ca557ed735c8f0110138eb62780c2e
Ryan Waweru <ryanwaweru475@gmail.com>
2025-05-24 21:18:24 +0300

    Add script to convert first argument to integer and print result

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,11 +1,3 @@
-const arg = process.argv[2];
-const number = parseInt(arg);
-
-if (isNaN(number)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${number}`);
-}
 const arg = process.argv[2];                 // get first argument
 const num = parseInt(arg, 10);               // convert to integer base 10
 
@@ -14,3 +6,4 @@ if (!isNaN(num)) {
 } else {
   console.log("Not a number");
 }
+
